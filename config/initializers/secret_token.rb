6df26313dbc7a1f22953cdb994b55bdc90b208eb
Jarek Kulikowski <jarek707@gmail.com>
2012-03-07 02:01:01 -0500

    RoR base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hs::Application.config.secret_token = '050e59f4db38e3dd38883ff36f8e77eabb6774ebc6337cf85de4dcf452fba00f1955572c2db9fad557fb626e3ed0e2cea412d7b325aea3ad491bb041d6282f93'
