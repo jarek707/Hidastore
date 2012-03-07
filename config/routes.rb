99ed97fbe8c5477ce62f314ec77139a6dcbf1fbd
Jarek Kulikowski <jarek707@gmail.com>
2012-03-07 04:15:03 -0500

    Some Stylesheet/HTML stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Hs::Application.routes.draw do
+  resources :users
+
+  resources :groups
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Hs::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
